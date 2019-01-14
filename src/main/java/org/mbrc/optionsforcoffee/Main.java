be8176e334335f42c7e4c2b73fb4a8b33ffd5065
Mriganka Basu Roy Chowdhury <mbrc12@gmail.com>
2019-01-15 02:44:19 +0530

    Fixed main's comments.

--- src/main/java/org/mbrc/optionsforcoffee/Main.java
+++ src/main/java/org/mbrc/optionsforcoffee/Main.java
@@ -10,39 +10,7 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     public static void main(String[] args) {
-//        GeometricBrownianMotion gbm = new GeometricBrownianMotion();
-//        INDArray samplePath = gbm.generateSamplePaths(1, 100).getRow(0l);
-//        System.out.println(Arrays.toString(samplePath.data().asDouble()));
-//
-//
-//        gbm = GeometricBrownianMotion.estimate(samplePath, 1);
-//
-//        System.out.println(gbm.getMu() + " " + gbm.getSigma());
-
-
-//        Interpreter interpreter = new Interpreter();
-//
-//        try {
-//            interpreter.set("foo", 9);
-//            interpreter.eval("bar = foo * 2; bar = Math.log(bar)");
-//            System.out.println(interpreter.get("bar"));
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-
         launch(args);
-
-//        LogNormalDistribution logNormalDistribution = new LogNormalDistribution(0.0, 10);
-
-//        System.out.println(logNormalDistribution.pdf(logNormalDistribution.mean()));
-
-
-//        GeometricBrownianMotion geometricBrownianMotion = new GeometricBrownianMotion(0.006, 0.015, 152.29);
-//        LogNormalDistribution logNormalDistribution = geometricBrownianMotion.getDistribution(6);
-
-//        System.out.println(logNormalDistribution.mean() + ", " + logNormalDistribution.std() + ", " + logNormalDistribution.sigma);
-
-//        System.out.println((logNormalDistribution.integrateOverDistribution((x) -> Math.max(x - 100.0, 0), Integrator.epsilon, Integrator.infinity))/Math.exp(0.0241 * 6 / 360));
     }
 
 
